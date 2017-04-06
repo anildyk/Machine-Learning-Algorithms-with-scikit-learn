2dd6195c76f289bd938e5052fb03829d21eb1ae3
ANIL KUMAR <anil946233@gmail.com>
2017-04-06 09:27:56 +0530

    Update ScrappyKNN.py

--- Classifiers/ScrappyKNN.py
+++ Classifiers/ScrappyKNN.py
@@ -6,13 +6,14 @@ class ScrappyKNN():
     def fit(self, X_train, y_train):
         self.X_train = X_train
         self.y_train = y_train
-    
+    #predict function for our model.
     def predict(self, X_test):
         predictions= []
         for row in X_test:
             label = self.closest(row)
             predictions.append(label)
         return predictions
+    #Function to calculate closest point to test data point i.e. row
     def closest(self, row):
         best_dist = euc(row, self.X_train[0])
         best_index=0
@@ -22,18 +23,19 @@ class ScrappyKNN():
                 best_dist = dist
                 best_index=i
         return self.y_train[best_index]
+#Load iris dataset from sklearn datasets
 from sklearn import datasets
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
-
+#Split data into two parts: 1) training data 2) test data
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
 
 #from sklearn.neighbors import KNeighborsClassifier
 #clf = KNeighborsClassifier()
 #Replacing KNN with our solution
-cl = ScrappyKNN()
+clf = ScrappyKNN()
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 #print predictions
