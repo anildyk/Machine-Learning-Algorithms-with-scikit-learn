2908537154e4bf8d1ac15554b49183b3f29a87b0
ANIL KUMAR <anil946233@gmail.com>
2017-04-21 12:03:52 +0530

    Load MovieLens data using pandas

---
+++ Fuzzy-Genetic Recommender System/fgrs.py
@@ -0,0 +1,19 @@
+import pandas as pd
+#Load data form MovieLens data
+#Load users
+u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
+users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols, encoding='latin-1')
+#load ratings
+r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
+ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols, encoding='latin-1')
+#load genres
+i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
+ 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
+ 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
+ encoding='latin-1')
+
+#Most rated movies
+most_rated = ratings.groupby('movie_id').size().sort_values(ascending=False)
+#Print top 50 movies sorted by number of ratings movies have.
+print most_rated[:50]
