de4b3992017dfb96dacff64e57f6371bf1d6cee7
ANIL KUMAR <anil946233@gmail.com>
2017-04-23 20:29:26 +0530

    Update with fuzzy sets and fizzy distance function

--- Fuzzy-Genetic Recommender System/fgrs.py
+++ Fuzzy-Genetic Recommender System/fgrs.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import numpy as np
+from fuzzy_sets import Age, GIM
+import gim
+
+def eucledian(A,B):
+    return np.linalg.norm(A-B)
+
+def fuzzy_dist(a, b, A, B):
+    return abs(a-b)*eucledian(A,B)
+#print fuzzy_dist(35, 40, np.array([3, 4 ,5]), np.array([4, 5, 6]))
+
 #Load data form MovieLens data
 #Load users
 u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
@@ -13,7 +24,20 @@ i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb 
 items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols,
  encoding='latin-1')
 
-#Most rated movies
-most_rated = ratings.groupby('movie_id').size().sort_values(ascending=False)
-#Print top 50 movies sorted by number of ratings movies have.
-print most_rated[:50]
+#Most rated movies (497 movies having # of movies rared >= 60)
+most_rated = ratings.groupby('movie_id').size().sort_values(ascending=False)[:497]  
+
+#Extract users those have rated most_rated movies
+users_top = [[]]
+for i in most_rated_movies:
+    users_top.append([ratings.loc[ratings['movie_id'] == i]['user_id']])
+user_tops = np.array([users_top])
+
+#Implementing fuzzy sets: class Age and GIM
+#GIM = Genre Interestingness Measure
+#Use young(), middle(), old() of Age class
+#Use very_bad(), bad(), average(), good(), very_good(), excellent() of GIM class
+a = Age()
+g = GIM()
+print g.very_bad(3), g.bad(3), g.average(4)
+print a.young(23) a.middle(23), a.old(45)
